539a16edf5415331ad9f10f75a0c306ffea15e5e
kjellski <otto.kjell@gmail.com>
2013-12-03 15:27:58 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngularBackend::Application.config.secret_key_base = 'bd5a87a36cec1439794132f35caa58fbd8b8c12dc63ff370b7930df0b3f13955194e7a9720d3acf92183ded76e414abb15693c5b390b846079f342cc33f3f262'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AngularBackend::Application.config.session_store :cookie_store, key: '_angular-backend_session'
